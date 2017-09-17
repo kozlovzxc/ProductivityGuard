dfc3e7b90ae7b0d306bf1b999cbf86dff605c3f3
Kozlov Nikita <kozlovzxc@gmail.com>
2017-09-17 23:18:00 +0300

    Add new option, trying to fix this shit

--- nope.js
+++ nope.js
@@ -1,22 +1,6 @@
-function removeCurrentUrl(){
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
-        current_url = tabs[0].url.substr(tabs[0].url.indexOf("#")+1)
-        current_hostname = getHostname(current_url);
-        chrome.storage.sync.get('forbidden_hostnames', function(result){
-            forbidden_hostnames = result['forbidden_hostnames'];
-            if (forbidden_hostnames)
-            {
-                index = $.inArray(current_hostname, forbidden_hostnames);
-                if (index !== -1)
-                    forbidden_hostnames.splice(index,1);
-            }
-            chrome.storage.sync.set(result);
-            chrome.tabs.update(tabs[0].id, {"url": current_url});
-        });
+$(document).ready(function () {
+    $("#proceedButton").click(function () {
+        removeCurrentUrl();
+        setTimeout(addCurrentUrl(), 5 * 60 * 1000); // add current Url after 5 minutes timeout
     });
-}
-
-$(document).ready(function() {
-    $("#proceedButton").click(removeCurrentUrl);
-    setTimeout(function(){$("#proceedButton").prop("disabled", false)}, 15*1000);
 });
--- popup.js
+++ popup.js
@@ -1,24 +1,3 @@
-function addCurrentUrl()
-{
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
-        current_hostname = getHostname(tabs[0].url);
-        chrome.storage.sync.get('forbidden_hostnames', function(result){
-            forbidden_hostnames = result['forbidden_hostnames'];
-            if (forbidden_hostnames)
-                if ($.inArray(current_hostname, forbidden_hostnames) === -1)
-                    forbidden_hostnames.push(current_hostname);
-                else
-                    alert('Current url is in the list');
-            else
-                forbidden_hostnames = [current_hostname];
-            chrome.storage.sync.set({'forbidden_hostnames': forbidden_hostnames}, function() {
-              alert("Current url has been added");
-            });
-            chrome.tabs.update(tabs[0].id, {"url": "nope.html#"+tabs[0].url});
-        });
-    });
-}
-
 function showForbiddenHostnames()
 {
     chrome.storage.sync.get('forbidden_hostnames', function(result){
@@ -29,5 +8,7 @@ function showForbiddenHostnames()
 window.onload = function()
 {
     $("#add").click(addCurrentUrl);
+    $("#remove").click(removeCurrentUrl);    
     $("#show").click(showForbiddenHostnames);
+    $("#clear").click(removeAllUrls);
 }
