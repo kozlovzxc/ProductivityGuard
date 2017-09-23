9416e3cdb2f978efc1887ff674885131c017ca17
Kozlov Nikita <kozlovzxc@gmail.com>
2017-09-24 00:23:10 +0300

    add elements ud

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../node_modules/element-ui/lib/theme-default/index.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import Elements from 'element-ui'
 
 Vue.config.productionTip = false
+
+Vue.use(Elements)
 
 /* eslint-disable no-new */
 new Vue({
